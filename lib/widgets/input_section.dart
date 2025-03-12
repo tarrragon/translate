2125f9b9fb3e299cac3a9abd0132bdb041142dfa
Tar <tarragonstop@gmail.com>
2025-03-12 22:02:03 +0800

    feat: 將 InputSection 重構為 ConsumerStatefulWidget，增強狀態管理
    - 將 InputSection 類別從 ConsumerWidget 轉換為 ConsumerStatefulWidget，以便更好地管理內部狀態
    - 新增 inputController 以控制文本輸入，並在狀態變更時更新其內容
    - 確保游標位置在文字末尾，提升使用者體驗
    - 明確設定文字方向為從左到右，增強可讀性

--- lib/widgets/input_section.dart
+++ lib/widgets/input_section.dart
@@ -3,19 +3,36 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../constants/app_strings.dart';
 import '../providers/translation_providers.dart';
 
-class InputSection extends ConsumerWidget {
+class InputSection extends ConsumerStatefulWidget {
   const InputSection({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<InputSection> createState() => _InputSectionState();
+}
+
+class _InputSectionState extends ConsumerState<InputSection> {
+  late TextEditingController inputController;
+
+  @override
+  void initState() {
+    super.initState();
+    inputController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    inputController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final inputText = ref.watch(inputTextProvider);
-    final TextEditingController inputController = TextEditingController(
-      text: inputText,
-    );
 
     // Update controller text if state changes
     if (inputController.text != inputText) {
       inputController.text = inputText;
+      // 確保游標位置在文字末尾
       inputController.selection = TextSelection.fromPosition(
         TextPosition(offset: inputController.text.length),
       );
@@ -31,8 +48,15 @@ class InputSection extends ConsumerWidget {
             border: const OutlineInputBorder(),
           ),
           maxLines: 5,
-          onChanged:
-              (value) => ref.read(inputTextProvider.notifier).state = value,
+          textDirection: TextDirection.ltr, // 明確設定文字方向為從左到右
+          onChanged: (value) {
+            // 更新 provider 狀態
+            ref.read(inputTextProvider.notifier).state = value;
+            // 確保游標位置在文字末尾
+            inputController.selection = TextSelection.fromPosition(
+              TextPosition(offset: inputController.text.length),
+            );
+          },
         ),
         const SizedBox(height: 16),
       ],
